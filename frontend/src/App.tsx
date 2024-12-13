43c5dd28720300e07b0dbf655c9a39d5dd1fb66b
kudaqwashe <kudaqwashem@gmail.com>
2024-12-13 18:37:16 +0200

    First commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/login";
+
+const App = () => {
+  return (
+    <div className="container">
+      <Login />
+    </div>
+  );
+};
+
+export default App;
